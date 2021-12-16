5e6a21e690e561573a852f83299c73e33f942be3
amolnagose12 <amolnagose12@gmail.com>
2021-12-16 23:20:12 +0530

    validate Name

--- src/main/java/bridgelabz/myFirstServlet/LoginServlet.java
+++ src/main/java/bridgelabz/myFirstServlet/LoginServlet.java
@@ -15,28 +15,20 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet
 (description = "Login Servlet Testing", 
-  urlPatterns = { "/LoginServlet" }, 
-  initParams = {
-		@WebInitParam(name = "user", value = "Amol"), 
-		@WebInitParam(name = "password", value = "Amol") })
+				urlPatterns = { "/LoginServlet" }, 	
+				initParams = {
+						@WebInitParam(name = "user", value = "Amol"), 
+						@WebInitParam(name = "password", value = "Amol@123") })
+
 public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-//		  Regex pattern for user name
-		 
 		String user = request.getParameter("user");
 		String namePattern = "^[A-Z]{1}[a-z]{3,}$";
 		Pattern pattern = Pattern.compile(namePattern);
 		Matcher match = pattern.matcher(user);
-		
-//		  Regex pattern for password
-		 
 		String pwd = request.getParameter("pwd");
-		String pass = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$";
-		Pattern patternPass = Pattern.compile(pass);
-		Matcher matchPass = pattern.matcher(pwd);
 		String userId = getServletConfig().getInitParameter("user");
 		String password = getServletConfig().getInitParameter("password");
 		if (userId.equals(user) && password.equals(pwd)) {
@@ -45,7 +37,7 @@ public class LoginServlet extends HttpServlet {
 		} else {
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
 			PrintWriter out = response.getWriter();
-			out.println("<font colour=red> Either User name or Password is Wrong</font>");
+			out.println("<font> Either User name or Password is Wrong</font>");
 			rd.include(request, response);
 		}
 	}
